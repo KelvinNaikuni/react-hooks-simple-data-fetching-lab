9839303233580a61edc6ae378eaedcb53aaedcac
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-10-31 08:08:36 +0300

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Send a fetch request to the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the state with the dog image URL and set isLoading to false
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+        setIsLoading(false);
+      });
+  }, []); // The empty array means this effect runs only once, similar to componentDidMount
+
+  return (
+    <div className="App">
+      <h1>Random Dog Image</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
